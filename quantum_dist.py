5f495eb1a452668c08bfab9c15000ec74dc2dc8d
Zach <z.thekauffman@gmail.com>
2020-02-09 13:38:39 -0600

    Added functions for fermi/bose distributions. Prompts for 3 temperatures then plots all of them on same axes.

--- quantum_dist.py
+++ quantum_dist.py
@@ -1,13 +1,22 @@
 import sys
-from scipy.stats import boltzmann
 import numpy as np
+import pylatex
 import matplotlib
 from matplotlib import pyplot as plt
 
+
+def fermi_dist(e, t):
+    return 1 / (np.exp(e / (k * t)) + 1)
+
+
+def bose_dist(e, t):
+    return 1 / (np.exp(e / (k * t)) - 1)
+
+
 matplotlib.use('TkAgg')
 
-k = 8.617 * pow(10, -5) # eV / degree Kelvin
-particle = 'uninitialized'
+k = 8.617 * pow(10, -5)  # eV / degree Kelvin
+particle = '   uninitialized'
 
 while 1 > 0:
     print("please enter the type of particle: ")
@@ -22,12 +31,40 @@ while 1 > 0:
         print(f'Unknown particle type: {line} Supported particles are [Fermion][Boson]')
     print(f'Selected particle is \'{particle}\'')
 
-    T = int(input("Enter the temperature: "))
+    T = int(input("Enter the first temperature to plot: "))
+    T2 = int(input("Enter the second temperature to plot: "))
+    T3 = int(input("Enter the third temperature to plot: "))
 
     if particle == 'Boson':
         f = np.zeros(400)
-        v = np.arange(0, 4, 0.01)
+        v = np.arange(.01, 4.01, 0.01)
         for i in np.arange(0, 400):
-            f[i] = 1 / (np.exp(v[i] / (k * T)) - 1)
-        plt.plot(v, f)
+            f[i] = bose_dist(v[i], T)
+        plt.plot(v, f, color='red')
+        for i in np.arange(0, 400):
+            f[i] = bose_dist(v[i], T2)
+        plt.plot(v, f, color='blue')
+        for i in np.arange(0, 400):
+            f[i] = bose_dist(v[i], T3)
+        plt.plot(v, f, color='green')
+        plt.xlim([0, 4.01])
+        plt.ylim([0, 2.5])
+        plt.title('Bose(-Einstein) distribution')
+        plt.show()
+
+    if particle == 'Fermion':
+        f = np.zeros(400)
+        v = np.arange(.01, 4.01, 0.01)
+        for i in np.arange(0, 400):
+            f[i] = fermi_dist(v[i], T)
+        plt.plot(v, f, color='red')
+        for i in np.arange(0, 400):
+            f[i] = fermi_dist(v[i], T2)
+        plt.plot(v, f, color='blue')
+        for i in np.arange(0, 400):
+            f[i] = fermi_dist(v[i], T3)
+        plt.plot(v, f, color='green')
+        plt.xlim([0, 4.01])
+        plt.ylim([0, 2.5])
+        plt.title('Fermi(-Dirac) distribution')
         plt.show()
